ac8a49471baac0b2dcfdbade60a0724c90e363ad
evanflorentino122020-blip <evanflorentino122020@gmail.com>
2026-06-27 13:13:24 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "stop"
+print(type(variable))
+variable = []
+print(type(variable))
